bd7caf4e506f291d903bc298a7281d1d36219c37
SaltedEggCalamari <149132331+SaltedEggCalamari@users.noreply.github.com>
2024-01-28 00:13:07 +0800

    Update question_1.py

--- question_1.py
+++ question_1.py
@@ -23,6 +23,7 @@ class flight:
 command_list = ['addpassenger', 'addflight', 'flightlist', 'flightrates', 'help', 'exit', '']
 command = ''
 passenger_list = []
+flight_list = []
 
 # Body code
 while command not in command_list or command != 'exit': # Repeats body until exit command is given
@@ -41,7 +42,7 @@ while command not in command_list or command != 'exit': # Repeats body until exi
     if command == "addpassenger":
         print("Command - addpassenger\n")
 
-        name        = input("Enter a name: ")
+        name        = input("Enter name: ")
         pp_number   = input("Enter passport number: ")
         DOB         = input("Enter Date of Birth: ")
         membership  = input("Enter membership status: ")
@@ -51,16 +52,42 @@ while command not in command_list or command != 'exit': # Repeats body until exi
         passenger_list.append(name)
         print("\nPassenger successfully created!\n"+
             "Passenger details as follows: \n"+
-            "Name: " + passenger_list[0].name +
-            "\nPassport number: " + passenger_list[0].pp_number +
-            "\nDOB: " + passenger_list[0].DOB + 
-            "\nMembership: " + passenger_list[0].membership + "\n\n")
+            "Name: " + name.name +
+            "\nPassport number: " + name.pp_number +
+            "\nDOB: " + name.DOB + 
+            "\nMembership: " + name.membership + "\n\n")
 
     elif command == "addflight":
         print("Command - addflight\n")
 
+        f_number    = input("Enter flight number: ")
+        cap         = input("Enter maximum capacity: ")
+        dep_date    = input("Enter departure date: ")
+        dep_time    = input("Enter departure time: ")
+        dep_loc     = input("Enter departure location: ")
+        arr_date    = input("Enter arrival date: ")
+        arr_time    = input("Enter arrival time: ")
+        arr_loc     = input("Enter arrival location: ")
+
+        dep = dep_arr(dep_date, dep_time, dep_loc)
+        arr = dep_arr(arr_date, arr_time, arr_loc)
+
+        f_number = flight(f_number, cap, dep, arr, [], [])
+
+        flight_list.append(f_number.f_number)
+
+        print("Flight successfully created!\n")
+        print("Flight number: " + f_number.f_number)
+        print("\nMaximum capacity: " + f_number.cap)
+        print("\nDeparting: " + f_number.dep.date + ", " + f_number.dep.time + " from " + f_number.dep.loc)
+        print("\nArrival: " + f_number.arr.date + ", " + f_number.arr.time + " at " + f_number.arr.loc)
+        
+
     elif command == "flightlist":
         print("Command - flightlist\n")
+        
+        print("Total number of flights: " + str(len(flight_list)) + "\n")
+        print(flight_list)
 
     elif command == "flightrates":
         print("Command - flightrates\n")
@@ -78,5 +105,4 @@ while command not in command_list or command != 'exit': # Repeats body until exi
 
     elif command not in command_list:
         print("Command not recognised. Please enter a recognised command.\n")
-
 
