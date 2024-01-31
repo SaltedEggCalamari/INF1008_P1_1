c5e2745a725350ecca01b413a500fb7c4f61214f
SaltedEggCalamari <149132331+SaltedEggCalamari@users.noreply.github.com>
2024-01-31 15:50:49 +0800

    Update question_1.py

--- question_1.py
+++ question_1.py
@@ -1,3 +1,7 @@
+# Imports
+import csv
+
+
 # Objects
 class passenger:
     def __init__(self, name, pp_number, DOB, membership):
@@ -11,19 +15,28 @@ class dep_arr:
         self.time = time
         self.loc = loc
 class flight:
-    def __init__(self, f_number, cap, dep, arr, conf_p, wait_p):
+    def __init__(self, f_number, cap, dep, arr, conf_pax, wait_pax):
         self.f_number = f_number
         self.cap = cap
         self.dep = dep
         self.arr = arr
-        self.conf_p = conf_p
-        self.wait_p = wait_p
+        self.conf_pax = conf_pax
+        self.wait_pax = wait_pax
+
 
 # Data initialization
-command_list = ['addpassenger', 'addflight', 'flightlist', 'flightrates', 'help', 'exit', '']
+command_list = ['addpassenger', 'addflight', 'loadfile', 'flightlist', 'passengerlist', 'flightrates', 'help', 'exit', '']
 command = ''
+
 passenger_list = []
+g_passenger_list = []
+s_passenger_list = []
+n_passenger_list = []
+
 flight_list = []
+conf_pax_list = []
+wait_pax_list = []
+
 
 # Body code
 while command not in command_list or command != 'exit': # Repeats body until exit command is given
@@ -39,28 +52,36 @@ while command not in command_list or command != 'exit': # Repeats body until exi
     command = input("Enter a command: ")
     print("Entered command: " + command)
 
-    if command == "addpassenger":
+    if command == "addpassenger": # (COMPLETED) Add new passenger to passenger list
         print("Command - addpassenger\n")
 
-        name        = input("Enter name: ")
+        name        = input("Enter name: ") # Get user input for passenger details
         pp_number   = input("Enter passport number: ")
         DOB         = input("Enter Date of Birth: ")
         membership  = input("Enter membership status: ")
 
-        name = passenger(name, pp_number, DOB, membership)
+        name = passenger(name, pp_number, DOB, membership) # Create new passenger object based on passenger name
 
-        passenger_list.append(name)
-        print("\nPassenger successfully created!\n"+
+        if name.membership == 'gold': # Checks and sorts passengers based on membership status
+            g_passenger_list.append(name.name)
+        elif name.membership == 'silver':
+            s_passenger_list.append(name.name)
+        elif name.membership == 'non':
+            n_passenger_list.append(name.name)
+
+        passenger_list = g_passenger_list + s_passenger_list + n_passenger_list # Collates passengers into a master name list that arrange according to membership status
+
+        print("\nPassenger successfully created!\n"+ # Feedback to check passenger creation
             "Passenger details as follows: \n"+
             "Name: " + name.name +
             "\nPassport number: " + name.pp_number +
             "\nDOB: " + name.DOB + 
             "\nMembership: " + name.membership + "\n\n")
 
-    elif command == "addflight":
+    elif command == "addflight": # (COMPLETED) Add new flight to flight list
         print("Command - addflight\n")
 
-        f_number    = input("Enter flight number: ")
+        f_number    = input("Enter flight number: ") # Gets user input for flight details
         cap         = input("Enter maximum capacity: ")
         dep_date    = input("Enter departure date: ")
         dep_time    = input("Enter departure time: ")
@@ -69,40 +90,111 @@ while command not in command_list or command != 'exit': # Repeats body until exi
         arr_time    = input("Enter arrival time: ")
         arr_loc     = input("Enter arrival location: ")
 
-        dep = dep_arr(dep_date, dep_time, dep_loc)
+        dep = dep_arr(dep_date, dep_time, dep_loc) # Creates dep_arr object to be inserted into flight object
         arr = dep_arr(arr_date, arr_time, arr_loc)
 
-        f_number = flight(f_number, cap, dep, arr, [], [])
+        f_number = flight(f_number, cap, dep, arr, [], []) # Creates flight object based on flight number
 
-        flight_list.append(f_number.f_number)
+        flight_list.append(f_number.f_number) # Adds flight number to flight list
 
-        print("Flight successfully created!\n")
+        print("Flight successfully created!\n") # Feedback to check flight creation
         print("Flight number: " + f_number.f_number)
         print("\nMaximum capacity: " + f_number.cap)
         print("\nDeparting: " + f_number.dep.date + ", " + f_number.dep.time + " from " + f_number.dep.loc)
-        print("\nArrival: " + f_number.arr.date + ", " + f_number.arr.time + " at " + f_number.arr.loc)
-        
+        print("\nArrival: " + f_number.arr.date + ", " + f_number.arr.time + " at " + f_number.arr.loc)   
 
-    elif command == "flightlist":
+    elif command == "loadfile": # (WORK IN PROGRESS) Loads passengers/flights from a .csv file
+        print("Command - loadpassengers\n")
+        # Entry types
+        #
+        # For passengers,
+        # P,(name),(pp_number),(DOB),(membership)
+        #
+        # For flights,
+        # F,(f_number),(cap),(dep date),(dep time),(dep loc),(arr date),(arr time),(arr loc)
+
+        filename = input("Enter filename: ") # Get filename to open
+        
+        file = open(filename)
+        reader = csv.reader(file) # Opens and reads file
+
+        for row in reader: # Cycles through data from csv row by row
+            if row[0] == 'P': # Passenger type entry handler
+                print("Passenger entry received\n")
+
+                name = row[1] # Gets passenger details from elements of row
+                pp_number = row[2]
+                DOB = row[3]
+                membership = row[4]
+
+                name = passenger(name, pp_number, DOB, membership) # Creates passenger object based on passenger name
+                print("Passenger successfully loaded\n")
+                print("Name: " + name.name +
+                      "\nPassport number: " + name.pp_number +
+                      "\nDOB: " + name.DOB +
+                      "\nMembership: " + name.DOB + "\n")
+
+                if name.membership == 'gold': # Sorts passenger into memberships
+                    g_passenger_list.append(name.name)
+                elif name.membership == 'silver':
+                    s_passenger_list.append(name.name)
+                elif name.membership == 'non':
+                    n_passenger_list.append(name.name)
+
+                passenger_list = g_passenger_list + s_passenger_list + n_passenger_list # Collates membership lists into a master name list
+
+            elif row[0] == 'F': # Flight type entry handler
+                print("Flight entry received\n")
+
+                f_number = row[1] # Get flight details from elements of row
+                cap = row[2]
+                dep_date = row[3]
+                dep_time = row[4]
+                dep_loc = row[5]
+                arr_date = row[6]
+                arr_time = row[7]
+                arr_loc = row [8]
+
+                dep = dep_arr(dep_date, dep_time, dep_loc) # Creates dep_arr object to be inserted into flight object
+                arr = dep_arr(arr_date, arr_time, arr_loc)
+
+                f_number = flight(f_number, cap, dep, arr, [], []) # Creates flight object based on flight number
+
+                flight_list.append(f_number.f_number) # Adds flight number to flight list
+
+
+            else: # Unknown entry handler
+                print("Unrecognised entry, try again\n")
+                break; # Stops loop once unknown entry is detected
+
+    elif command == "flightlist": # (COMPLETED) View all flights
         print("Command - flightlist\n")
         
         print("Total number of flights: " + str(len(flight_list)) + "\n")
         print(flight_list)
 
-    elif command == "flightrates":
+    elif command == "passengerlist": # (COMPLETED) View all passengers
+        print("Command - passengerlist\n")
+        
+        print("Total number of passengers: " + str(len(passenger_list)) + "\n")
+        print(passenger_list)
+
+    elif command == "flightrates": # (WORK IN PROGRESS) View flights with highest occupancies rates
         print("Command - flightrates\n")
 
-    elif command == "help":
+    elif command == "help": # (WORK IN PROGRESS) Normal Help function
         print("Command - help\n")
 
-    elif command == "exit":
+    elif command == "exit": # (COMPLETED) Cease program function
         print("Command - exit\n")
 
-        print("All data will be lost on exit.\n")
-        exit_confirm = input("To confirm you wish to exit, enter \"CONFIRM\": ")
+        exit_confirm = input("To confirm you wish to exit, enter \"CONFIRM\": ") # Confirms users request to exit program
         if exit_confirm != "CONFIRM":
             command = ""
 
-    elif command not in command_list:
+    elif command not in command_list: # Loop to start sequence
         print("Command not recognised. Please enter a recognised command.\n")
+
+    # (WORK IN PROGRESS) Algorithms go here
+                
 
